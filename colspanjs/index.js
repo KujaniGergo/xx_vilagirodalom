f2f8112a16618358d7f2ffc718ef19f1b7f3f4f5
kujani gergo <kujani.gergo_2022c@bolyai-szakkozep.hu>
2026-01-07 10:05:05 -0800

    console col valtozo bev

--- colspanjs/index.js
+++ colspanjs/index.js
@@ -1,12 +1,136 @@
-//commit 1. console col tablazat
-console.log("Szerző  | Mű | Fogalmak | |"); //címsor
+//commit 2.  console col valtozo bev
+//címsor
+/**
+ * @type {string} Címsor szerző típusa
+ */
+const cimSzerzo = "Szerző" //Fejléc szerző változó deklarálása
 
-console.log("Appolliniare | A megsebzett galamb és a szökőkút | képvers | emlékezés |"); //1. sor
+/**
+ * @type {string} Címsor Mű típusa
+ */
+const cimMu = "Mű" //Fejléc mű deklarálása
 
-console.log("Apollinaire | Búcsú | avantgárd |  |"); //2. sor
+/**
+ * @type {string} Címsor Fogalmak típusa
+ */
+const cimFogalmak = "Fogalmak"//Fejléc fogalmak deklarálása
 
-console.log("Thomas Mann | Mario és a varázsló | kisregény |  |"); //3.sor
 
-console.log("Franz Kafka | A per | regény |  |"); //4. sor
 
-console.log("Franz Kafka | Az átvlátozás | kisregény | groteszk |"); //5.sor
+
+//első sor
+/**
+ * @type {string} Első sor szerző típusa
+ */
+const elsoSorSzerzo = "Appolliniare" //Első sor szerző változó deklarálása
+
+/**
+ * @type {string} Első mű szerző típusa
+ */
+const elsoSorMu = "A megsebzett galamb és a szökőkút" //első sor szerző változó deklarálása
+
+/**
+ * @type {string} Első fogalmak1 szerző típusa
+ */
+const elsoSorFogalmakEgy = "képvers" //első fogalmak1 szerző változó deklarálása
+
+/**
+ * @type {string} Első fogalmak2 szerző típusa
+ */
+const elsoSorFogalmakKetto = "emlékezés" //első fogalmak2 szerző változó deklarálása
+
+
+
+
+//második sor
+/**
+ * @type {string} Második sor szerző típusa
+ */
+const masodikSorSzerzo = "Appolliniare" //Második sor szerző változó deklarálása
+
+/**
+ * @type {string} Második mű szerző típusa
+ */
+const masdikSorMu = "Búcsú" //Második sor szerző változó deklarálása
+
+/**
+ * @type {string} Második fogalmak1 szerző típusa
+ */
+const masodikSorFogalmakEgy = "avantgárd" //Második fogalmak1 szerző változó deklarálása
+
+
+
+
+//harmadik sor
+/**
+ * @type {string} Harmadik sor szerző típusa
+ */
+const harmadikSorSzerzo = "Thomas Mann" //Harmadik sor szerző változó deklarálása
+
+/**
+ * @type {string} Harmadik mű szerző típusa
+ */
+const harmadikSorMu = "Mario és a varázsló" //Harmadik sor szerző változó deklarálása
+
+/**
+ * @type {string} Harmadik fogalmak1 szerző típusa
+ */
+const harmadikSorFogalmakEgy = "kisregény" //Harmadik fogalmak1 szerző változó deklarálása
+
+
+
+
+
+//negyedik sor
+/**
+ * @type {string} Negyedik sor szerző típusa
+ */
+const negyedikSorSzerzo = "Franz Kafka" //Negyedik sor szerző változó deklarálása
+
+/**
+ * @type {string} Negyedik mű szerző típusa
+ */
+const negyedikSorMu = "A per" //Negyedik sor szerző változó deklarálása
+
+/**
+ * @type {string} Negyedik fogalmak1 szerző típusa
+ */
+const negyedikSorFogalmakEgy = "regény" //Negyedik fogalmak1 szerző változó deklarálása
+
+
+
+
+
+//ötödik sor
+/**
+ * @type {string} Ötödik sor szerző típusa
+ */
+const otodikSorSzerzo = "Franz Kafka" //Ötödik sor szerző változó deklarálása
+
+/**
+ * @type {string} Ötödik mű szerző típusa
+ */
+const otodikSorMu = "Az átvlátozás" //Ötödik sor szerző változó deklarálása
+
+/**
+ * @type {string} Ötödik fogalmak1 szerző típusa
+ */
+const otodikSorFogalmakEgy = "kisregény" //Ötödik fogalmak1 szerző változó deklarálása
+
+/**
+ * @type {string} Ötödik fogalmak1 szerző típusa
+ */
+const otodikSorFogalmakKetto = "groteszk" //Ötödik fogalmak2 szerző változó deklarálása
+
+//kiírás consol-ba
+console.log( cimSzerzo +" | "+ cimMu +" | " + cimFogalmak +"| |" );// sor logolás
+
+console.log(`${elsoSorSzerzo} | ${elsoSorMu} | ${elsoSorFogalmakEgy} | ${elsoSorFogalmakKetto} |`) //1. sor logolás
+
+console.log(`${masodikSorSzerzo} | ${masdikSorMu} | ${masodikSorFogalmakEgy} | |`) //2. sor logolás
+
+console.log(`${harmadikSorSzerzo} | ${harmadikSorMu} | ${harmadikSorFogalmakEgy} |  |`)//3. sor logolás
+
+console.log(`${negyedikSorSzerzo} | ${negyedikSorMu} | ${negyedikSorFogalmakEgy} | |`) //4. sor logolás
+
+console.log(`${otodikSorSzerzo} | ${otodikSorMu} | ${otodikSorFogalmakEgy} | ${otodikSorFogalmakKetto} |`)//5. sor logolás
