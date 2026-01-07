44faae408821b24abc468449c203028d34ac655e
kujani gergo <kujani.gergo_2022c@bolyai-szakkozep.hu>
2026-01-07 09:59:43 -0800

    console col tablazat

---
+++ colspanja/index.js
@@ -0,0 +1,12 @@
+//commit 1. console col tablazat
+console.log("Szerző  | Mű | Fogalmak | |"); //címsor
+
+console.log("Appolliniare | A megsebzett galamb és a szökőkút | képvers | emlékezés |"); //1. sor
+
+console.log("Apollinaire | Búcsú | avantgárd |  |"); //2. sor
+
+console.log("Thomas Mann | Mario és a varázsló | kisregény |  |"); //3.sor
+
+console.log("Franz Kafka | A per | regény |  |"); //4. sor
+
+console.log("Franz Kafka | Az átvlátozás | kisregény | groteszk |"); //5.sor
