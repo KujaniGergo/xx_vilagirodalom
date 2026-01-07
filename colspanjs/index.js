3cbcc2ad99a21a5519edae62d1e73f0df52065d1
kujani gergo <kujani.gergo_2022c@bolyai-szakkozep.hu>
2026-01-07 10:21:32 -0800

    console col ciklus

--- colspanjs/index.js
+++ colspanjs/index.js
@@ -1,4 +1,4 @@
-// commit 5.  console col elagazas
+//commit 6. console col ciklus
 /**
  * @type {string[]} a cimSorhoz szukseges tomb
  */
@@ -37,72 +37,20 @@ const adatTomb = [ // tomb deklaralasa
     }
 ]
 
-// A string konkatenaciora olyan szintaxist hasznalsz amilyet akarsz
-console.log(cimSor[0] + " | " + cimSor[1] + " | " + cimSor[2] + " |"); // Kiirom konzolra a tablazat fejlecet
 
-/**
- * @type {string} Elso soranak osszeallitott tartalma
- */
-let elsoSor = `${adatTomb[0].szerzo} | ${adatTomb[0].mu} | `; //Feltöltjük elemekkel
-if(adatTomb[0].fogalmakKetto != undefined){ //Megnézzük hogy van e "fogalomKetto"
-    elsoSor += `${adatTomb[0].fogalmakEgy} | ${adatTomb[0].fogalmakKetto} |`; //Ha nincsen akkor cellák összevonása
-} else { //Elágazás
-    elsoSor += `${adatTomb[0].fogalmakEgy} |`; //Cella hozzáfűzésa
+console.log(cimSor[0] +" | "+ cimSor[1] +" | " + cimSor[2] +" |"); //Címsor kiíréas
+
+for(const obj of adatTomb){  //For cikuls a tömb bejárására
+    /**
+     * @type {string} Típus megadása
+     */
+    let Sor = `${obj.szerzo} | ${obj.mu} |`; //Feltöltés értékekkel
+    if(obj.fogalmakKetto != undefined) //vizsgaljuk, hogy az "FogalmakKetto" undefine-e
+    { 
+        Sor += `${obj.fogalmakEgy} | ${obj.fogalmakKetto} |`; //Ha igen akkor cellák összefűzése
+    }else //elágazás
+    { 
+        Sor += `${obj.fogalmakEgy} |`; //Cella hozzáfűzése
+    }
+    console.log(Sor) //Kiírás
 }
-console.log(elsoSor); //Kiírás
-
-
-
-
-/**
- * @type {string} Második soranak osszeallitott tartal  ma
- */
-let masodikSor = `${adatTomb[1].szerzo} | ${adatTomb[1].mu} | `; //Feltöltjük elemekkel
-if(adatTomb[1].fogalmakKetto != undefined){ //Megnézzük hogy van e "fogalomKetto"
-    masodikSor += `${adatTomb[1].fogalmakEgy} | ${adatTomb[1].fogalmakKetto} |`; //Ha nincsen akkor cellák összevonása
-} else { //Elágazás
-    masodikSor += `${adatTomb[1].fogalmakEgy} |`; //Cella hozzáfűzésa
-}
-console.log(masodikSor); //Kiírás
-
-
-
-
-/**
- * @type {string} Harmadik soranak osszeallitott tartalma
- */
-let harmadikSor = `${adatTomb[2].szerzo} | ${adatTomb[2].mu} | `; //Feltöltjük elemekkel
-if(adatTomb[2].fogalmakKetto != undefined){ //Megnézzük hogy van e "fogalomKetto"
-    harmadikSor += `${adatTomb[2].fogalmakEgy} | ${adatTomb[2].fogalmakKetto} |`; //Ha nincsen akkor cellák összevonása
-} else { //Elágazás
-    harmadikSor += `${adatTomb[2].fogalmakEgy} |`; //Cella hozzáfűzésa
-}
-console.log(harmadikSor); //Kiírás
-
-
-
-
-/**
- * @type {string} Negyedik soranak osszeallitott tartalma
- */
-let negyedikSor = `${adatTomb[3].szerzo} | ${adatTomb[3].mu} | `; //Feltöltjük elemekkel
-if(adatTomb[3].fogalmakKetto != undefined){ //Megnézzük hogy van e "fogalomKetto"
-    negyedikSor += `${adatTomb[3].fogalmakEgy} | ${adatTomb[3].fogalmakKetto} |`; //Ha nincsen akkor cellák összevonása
-} else { //Elágazás
-    negyedikSor += `${adatTomb[3].fogalmakEgy} |`; //Cella hozzáfűzésa
-}
-console.log(negyedikSor); //Kiírás
-
-
-
-
-/**
- * @type {string} Ötödik soranak osszeallitott tartalma
- */
-let otodikSor = `${adatTomb[4].szerzo} | ${adatTomb[4].mu} | `; //Feltöltjük elemekkel
-if(adatTomb[4].fogalmakKetto != undefined){ //Megnézzük hogy van e "fogalomKetto"
-    otodikSor += `${adatTomb[4].fogalmakEgy} | ${adatTomb[4].fogalmakKetto} |`; //Ha nincsen akkor cellák összevonása
-} else {//Elágazás 
-    otodikSor += `${adatTomb[4].fogalmakEgy} |`; //Cella hozzáfűzésa
-}
-console.log(otodikSor); //Kiírás
