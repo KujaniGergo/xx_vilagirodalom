d97ea1423379f13848a04bdcf51e4dce7651a005
kujani gergo <kujani.gergo_2022c@bolyai-szakkozep.hu>
2026-01-08 17:44:53 -0800

    console row object bev

--- rowspanjs/index.js
+++ rowspanjs/index.js
@@ -1,11 +1,91 @@
-console.log("Szerző  | Mű | Fogalmak |"); //címsor
+/**
+ * @type {string} A szerző típusa
+ */
+const fejlecSzerzo = "Szerző" //Fejléc sor adata
+/**
+ * @type {string} A mű típusa
+ */
+const fejlecMu = "Mű" //Fejléc sor adata
+/**
+ * @type {string} A fogalmak típusa
+ */ 
+const fejlecFogalmak = "Fogalmak" //Fejléc sor adata
 
-console.log("Appolliniare | A megsebzett galamb és a szökőkút | képvers |"); //1.sor
 
-console.log("Apollinaire | Búcsú | avantgárd | emlékezésl |"); //2.sor
+/**
+ * @type {string} Első sor szerzője
+ */
+const elsoSorSzerzo = "Apollinaire" //Első sor adata
+/**
+ * @type {string} Első sor mű címe
+ */
+const elsoSorMu = "A megsebzett galamb és a szökőkút" //Első sor adata
+/**
+ * @type {string} Első sor fogalma
+ */
+const elsoSorFogalom = "képvers" //Első sor adata
 
-console.log("Thomas Mann | Mario és a varázsló | kisregény |"); //3.sor
 
-console.log("Franz Kafka | A per | regény |"); //4.sor
+/**
+ * @type {string} Második sor szerzője
+ */
+const masodikSorSzerzo = "Apollinaire" //Második sor adata
+/**
+ * @type {string} Második sor mű címe
+ */
+const masodikSorMu = "Búcsú" //Második sor adata
+/**
+ * @type {string} Második sor fogalma
+ */
+const masodikSorFogalom = "avantgárd" //Második sor adata
 
-console.log("Franz Kafka | Az átvlátozás | kisregény |"); //5.sor
+
+/**
+ * @type {string} Harmadik sor szerzője
+ */
+const harmadikSorSzerzo = "Thomas Mann" //Harmadik sor adata
+/**
+ * @type {string} Harmadik sor mű címe
+ */
+const harmadikSorMu = "Mario és a varázsló" //Harmadik sor adata
+/**
+ * @type {string} Harmadik sor fogalma
+ */
+const harmadikSorFogalom = "kisregény" //Harmadik sor adata
+
+
+/**
+ * @type {string} Negyedik sor szerzője
+ */
+const negyedikSorSzerzo = "Franz Kafka" //Negyedik sor adata
+/**
+ * @type {string} Negyedik sor mű címe
+ */
+const negyedikSorMu = "A per" //Negyedik sor adata
+/**
+ * @type {string} Negyedik sor fogalma
+ */
+const negyedikSorFogalom = "regény" //Negyedik sor adata
+
+
+/**
+ * @type {string} Ötödik sor szerzője
+ */
+const otodikSorSzerzo = "Franz Kafka" //Ötödik sor adata
+/**
+ * @type {string} Ötödik sor mű címe 
+ */
+const otodikSorMu = "Az átváltozás" //Ötödik sor adata
+/**
+ * @type {string} Ötödik sor fogalma
+ */
+const otodikSorFogalom = "kisregény" //Ötödik sor adata
+
+
+// Táblázat kiírása a konzolba
+console.log(`${fejlecSzerzo} | ${fejlecMu} | ${fejlecFogalmak} |`) //Fejléc Kiírás
+console.log(`${elsoSorSzerzo} | ${elsoSorMu} | ${elsoSorFogalom} |`) //1. sor Kiírás
+console.log(`${masodikSorSzerzo} | ${masodikSorMu} | ${masodikSorFogalom} |`) //2. sor Kiírás
+console.log(`${harmadikSorSzerzo} | ${harmadikSorMu} | ${harmadikSorFogalom} |`) //3. sor Kiírás
+console.log(`${negyedikSorSzerzo} | ${negyedikSorMu} | ${negyedikSorFogalom} |`) //4. sor Kiírás
+console.log(`${otodikSorSzerzo} | ${otodikSorMu} | ${otodikSorFogalom} |`) //5. sor Kiírás
