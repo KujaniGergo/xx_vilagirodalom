ada3a781dcc458f2adcf5b0cf18487e9f74b78ba
kujani gergo <kujani.gergo_2022c@bolyai-szakkozep.hu>
2026-01-08 18:59:30 -0800

    console row array bev

--- rowspanjs/index.js
+++ rowspanjs/index.js
@@ -1,53 +1,38 @@
 /**
- * @type {string} A szerző oszlopfejléce
+ * @type {string[]} A szerzők táblázatának oszlopfejlécei
  */
-const fejlecSzerzo = "Szerző" // változó deklarálása
-/**
- * @type {string} A mű oszlopfejléce
- */
-const fejlecMu = "Mű" // változó deklarálása
-/**
- * @type {string} A fogalom oszlopfejléce
- */
-const fejlecFogalom = "Fogalom" // változó deklarálása
-
+const cimSor = ["Szerző", "Mű", "Fogalom"] // cimSor változó deklarálása a fejléceknek
 
 /**
- * @type {{szerzo: string, mu: string, fogalmakEgy: string, muKetto: string, fogalmakKetto: string}} Apollinairehez tartozó adatok
+ * @type {{szerzo: string, muEgy: string, fogalomEgy: string, muKetto: string, fogalomKetto: string}[]} //Adattömb amiből lesz a táblázat
  */
-const szerzoApollinaire = { // objektum definiáls
-    szerzo: "Apollinaire", // szerző neve
-    mu: "A megsebzett galamb és a szökőkút", // első mű
-    fogalmakEgy: "képvers", // első mű fogalma
-    muKetto: "Búcsú", // második mű
-    fogalmakKetto: "avantgárd" // második mű fogalma
-}
-
-/**
- * @type {{szerzo: string, mu: string, fogalmakEgy: string}} Mannhez tartozó adatok
- */
-const szerzoThomasMann = { // objektum definiáls
-    szerzo: "Thomas Mann", // szerző neve
-    mu: "Mario és a varázsló", // mű
-    fogalmakEgy: "kisregény" // mű fogalma
-}
-
-/**
- * @type {{szerzo: string, mu: string, fogalmakEgy: string, muKetto: string, fogalmakKetto: string}} Kafkahoz tartozó adatok
- */
-const szerzoKafka = { // objektum definiálás
-    szerzo: "Franz Kafka", // szerző neve
-    mu: "A per", // első mű
-    fogalmakEgy: "regény", // első mű fogalma
-    muKetto: "Az átváltozás", // második mű
-    fogalmakKetto: "kisregény" // második mű fogalma
-}
+const adatTomb = [ //Tömb definiálása
+    { // objektum definiálása Apollinaire
+        szerzo: "Apollinaire", // szerző neve
+        muEgy: "A megsebzett galamb és a szökőkút", // első mű
+        fogalomEgy: "képvers", // első mű fogalma
+        muKetto: "Búcsú", // második mű
+        fogalomKetto: "avantgárd" // második mű fogalma
+    },
+    { // objektum definiálása Thomas Mann
+        szerzo: "Thomas Mann", // szerző neve
+        muEgy: "Mario és a varázsló", // mű címe
+        fogalomEgy: "kisregény" // mű fogalma
+    },
+    { // objektum definiálása Franz Kafka
+        szerzo: "Franz Kafka", // szerző neve
+        muEgy: "A per", // első mű
+        fogalomEgy: "regény", // első mű fogalma
+        muKetto: "Az átváltozás", // második mű
+        fogalomKetto: "kisregény" // második mű fogalma
+    }
+]
 
 
-// Táblázat kiírása a konzolba
-console.log(`${fejlecSzerzo} | ${fejlecMu} | ${fejlecFogalom} |`) // Fejléc kiírása
-console.log(`${szerzoApollinaire.szerzo} | ${szerzoApollinaire.mu} | ${szerzoApollinaire.fogalmakEgy} |`) // 1. sor
-console.log(` |_ | ${szerzoApollinaire.muKetto} | ${szerzoApollinaire.fogalmakKetto} |`) // 2. sor
-console.log(`${szerzoThomasMann.szerzo} | ${szerzoThomasMann.mu} | ${szerzoThomasMann.fogalmakEgy} |`) // 3. sor
-console.log(`${szerzoKafka.szerzo} | ${szerzoKafka.mu} | ${szerzoKafka.fogalmakEgy} |`) // 4. sor
-console.log(` |_ | ${szerzoKafka.muKetto} | ${szerzoKafka.fogalmakKetto} |`) // 5. sor
+console.log(`${cimSor[0]} | ${cimSor[1]} | ${cimSor[2]} |`) // Kiírom konzolra a táblázat fejlécét
+console.log(`${adatTomb[0].szerzo} | ${adatTomb[0].muEgy} | ${adatTomb[0].fogalomEgy} |`) //1. sor
+console.log(` |_ | ${adatTomb[0].muKetto} | ${adatTomb[0].fogalomKetto} |`) //2. sorát
+console.log(`${adatTomb[1].szerzo} | ${adatTomb[1].muEgy} | ${adatTomb[1].fogalomEgy} |`) //3. sor
+console.log(`${adatTomb[2].szerzo} | ${adatTomb[2].muEgy} | ${adatTomb[2].fogalomEgy} |`) //4. sor
+console.log(` |_ | ${adatTomb[2].muKetto} | ${adatTomb[2].fogalomKetto} |`) //1. sor
+ 
