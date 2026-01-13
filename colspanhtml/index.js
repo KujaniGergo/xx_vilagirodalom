309f2b3150c7b149e03b01c22505e0c0c6077266
kujani gergo <kujani.gergo_2022c@bolyai-szakkozep.hu>
2026-01-12 17:51:48 -0800

    col html double esemeny impl

--- colspanhtml/index.js
+++ colspanhtml/index.js
@@ -9,9 +9,9 @@ szimpla.addEventListener('click', function(){ //event listener szimpla gomb
      * @type {dataType[]} //lista típusa
      */
     const ujSor = { //uj cimsor 
-        szerzo: "Szerző", //szerző
-        mu: "Mű", //mű
-        fogalmak: "Fogalmak" // fogalmnak
+        szerzo: "szerző példa", //szerző
+        mu: "mű példa", //mű
+        fogalmak: "fogalmak példa1" // fogalmnak
     }
 
     /**
@@ -28,38 +28,24 @@ szimpla.addEventListener('click', function(){ //event listener szimpla gomb
     /**
      * @type {HTMLTableCellElement} cimsor szező cella
      */
-    const thSzerzo = document.createElement("th"); //cimsor szerző cella
-    tr.appendChild(thSzerzo); //sorhozfűzés
-    thSzerzo.innerText = ujSor.szerzo //feltöltés az új sor adataibol
+    const tdSzerzo = document.createElement("td"); //cimsor szerző cella
+    tr.appendChild(tdSzerzo); //sorhozfűzés
+    tdSzerzo.innerText = ujSor.szerzo //feltöltés az új sor adataibol
 
     /**
      * @type {HTMLTableCellElement} cimsor mű cella
      */
-    const thMu = document.createElement("th"); //cimsor mű cella
-    tr.appendChild(thMu); //sorhozfűzés
-    thMu.innerText = ujSor.mu //feltöltés az új sor adataibol
+    const tdMu = document.createElement("td"); //cimsor mű cella
+    tr.appendChild(tdMu); //sorhozfűzés
+    tdMu.innerText = ujSor.mu //feltöltés az új sor adataibol
 
     /**
      * @type {HTMLTableCellElement} cimsor fogalmak cella
      */
-    const thFogalmak = document.createElement("th"); // cimsor fogalmak cella
-    tr.appendChild(thFogalmak); //sorhozfűzés 
-    thFogalmak.innerText = ujSor.fogalmak //feltöltés az új sor adataibol
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    const tdFogalmak = document.createElement("td"); // cimsor fogalmak cella
+    tr.appendChild(tdFogalmak); //sorhozfűzés 
+    tdFogalmak.innerText = ujSor.fogalmak //feltöltés az új sor adataibol
+});
 
 
 //dupla gomb event listener
@@ -69,4 +55,56 @@ szimpla.addEventListener('click', function(){ //event listener szimpla gomb
 const dupla = document.getElementById('dupla') //gomb element lekérése
 dupla.addEventListener('click', function(){ //event listener dupla gomb
     console.log("dupla gomb") //ha le van nyomva ezt logolja
+
+    /**
+     * @type {dataType[]} //lista típusa
+     */
+    const ujSor = { //uj cimsor 
+        szerzo: "szerző példa", //szerző
+        mu: "mű példa ", //mű
+        fogalmakEgy: "fogalmak1 példa", // fogalmnak
+        fogalmakKetto: "fogalmak2 példa" // fogalmnak
+
+    }
+
+    /**
+     * @type {HTMLBodyElement} lekért body típusa
+     */
+    const tbody = document.getElementById("tablebody"); //body lekérése
+
+    /**
+     * @type {HTMLTableRowElement} sor típusa
+     */
+    const tr = document.createElement("tr"); // sor létrehozása
+    tbody.appendChild(tr); //sor bodyhoz fűzése
+
+    /**
+     * @type {HTMLTableCellElement} cimsor szező cella
+     */
+    const tdSzerzo = document.createElement("td"); //cimsor szerző cella
+    tr.appendChild(tdSzerzo); //sorhozfűzés
+    tdSzerzo.innerText = ujSor.szerzo //feltöltés az új sor adataibol
+
+    /**
+     * @type {HTMLTableCellElement} cimsor mű cella
+     */
+    const tdMu = document.createElement("td"); //cimsor mű cella
+    tr.appendChild(tdMu); //sorhozfűzés
+    tdMu.innerText = ujSor.mu //feltöltés az új sor adataibol
+
+    /**
+     * @type {HTMLTableCellElement} cimsor fogalmak cella
+     */
+    const tdFogalmakEgy = document.createElement("td"); // cimsor fogalmak cella
+    tr.appendChild(tdFogalmakEgy); //sorhozfűzés 
+    tdFogalmakEgy.innerText = ujSor.fogalmakEgy //feltöltés az új sor adataibol
+
+
+    /**
+     * @type {HTMLTableCellElement} cimsor fogalmak cella
+     */
+    const tdFogalmakKetto = document.createElement("td"); // cimsor fogalmak cella
+    tr.appendChild(tdFogalmakKetto); //sorhozfűzés 
+    tdFogalmakKetto.innerText = ujSor.fogalmakKetto //feltöltés az új sor adataibol
 })
+
